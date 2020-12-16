42779242b9b316d2f5bc29e11e327a10cbc549c0
Mike Matthews <mike@factor-50.co.uk>
2020-12-16 16:37:05 +0000

    GPS-353 Adding questions for North Muskham request flow

--- RequestService/RequestService.Repo/Migrations/20201216163622_NortMuskhamRequestFlow.Designer.cs
+++ RequestService/RequestService.Repo/Migrations/20201216163622_NortMuskhamRequestFlow.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RequestService.Repo;
 
 namespace RequestService.Repo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201216163622_NortMuskhamRequestFlow")]
+    partial class NortMuskhamRequestFlow
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
