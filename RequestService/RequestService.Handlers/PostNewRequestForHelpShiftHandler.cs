c5a17face84c98b57edd29cf064030021611d307
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-12-22 13:57:24 +0000

    add unit tests

--- RequestService/RequestService.Handlers/PostNewRequestForHelpShiftHandler.cs
+++ RequestService/RequestService.Handlers/PostNewRequestForHelpShiftHandler.cs
@@ -18,39 +18,13 @@ namespace RequestService.Handlers
     public class PostNewRequestForHelpShiftHandler : IRequestHandler<PostNewRequestForHelpShiftRequest, PostNewRequestForHelpShiftResponse>
     {
         private readonly IRepository _repository;
-        private readonly ICommunicationService _communicationService;
-        private readonly IUserService _userService;
-        private readonly IAddressService _addressService;
         private readonly IGroupService _groupService;
-        private readonly IOptionsSnapshot<ApplicationConfig> _applicationConfig;
         public PostNewRequestForHelpShiftHandler(
-            IRepository repository,
-            IUserService userService,
-            IAddressService addressService,
-            ICommunicationService communicationService,
-            IGroupService groupService,
-            IOptionsSnapshot<ApplicationConfig> applicationConfig)
+            IRepository repository,          
+            IGroupService groupService)
         {
             _repository = repository;
-            _userService = userService;
-            _addressService = addressService;
-            _communicationService = communicationService;
             _groupService = groupService;
-            _applicationConfig = applicationConfig;
-        }
-
-        private void CopyRequestorAsRecipient(PostNewRequestForHelpRequest request)
-        {
-            request.HelpRequest.Requestor.Address.Postcode = HelpMyStreet.Utils.Utils.PostcodeFormatter.FormatPostcode(request.HelpRequest.Requestor.Address.Postcode);
-
-            if (request.HelpRequest.RequestorType == RequestorType.Myself)
-            {
-                request.HelpRequest.Recipient = request.HelpRequest.Requestor;                
-            }
-            else
-            {
-                request.HelpRequest.Recipient.Address.Postcode = HelpMyStreet.Utils.Utils.PostcodeFormatter.FormatPostcode(request.HelpRequest.Recipient.Address.Postcode);
-            }
         }
 
         public async Task<PostNewRequestForHelpShiftResponse> Handle(PostNewRequestForHelpShiftRequest request, CancellationToken cancellationToken)
@@ -68,32 +42,32 @@ namespace RequestService.Handlers
                 };
             }
 
-            //if(formVariant.AccessRestrictedByRole)
-            //{
-            //    bool failedChecks = request.CreatedByUserId == 0;
+            if (formVariant.AccessRestrictedByRole)
+            {
+                bool failedChecks = request.CreatedByUserId == 0;
 
-            //    if(!failedChecks)
-            //    {
-            //        var groupMember = await _groupService.GetGroupMember(new HelpMyStreet.Contracts.GroupService.Request.GetGroupMemberRequest()
-            //        {
-            //            AuthorisingUserId = request.CreatedByUserId,
-            //            UserId = request.CreatedByUserId,
-            //            GroupId = request.ReferringGroupId
-            //        });
+                if (!failedChecks)
+                {
+                    var groupMember = await _groupService.GetGroupMember(new HelpMyStreet.Contracts.GroupService.Request.GetGroupMemberRequest()
+                    {
+                        AuthorisingUserId = request.CreatedByUserId,
+                        UserId = request.CreatedByUserId,
+                        GroupId = request.ReferringGroupId
+                    });
 
-            //        failedChecks = !groupMember.UserInGroup.GroupRoles.Contains(GroupRoles.RequestSubmitter);
-            //    }
+                    failedChecks = !groupMember.UserInGroup.GroupRoles.Contains(GroupRoles.RequestSubmitter);
+                }
 
-            //    if (failedChecks)
-            //    {
-            //        return new PostNewRequestForHelpShiftResponse
-            //        {
-            //            RequestID = -1,
-            //            Fulfillable = Fulfillable.Rejected_Unauthorised
-            //        };
-            //    }
+                if (failedChecks)
+                {
+                    return new PostNewRequestForHelpShiftResponse
+                    {
+                        RequestID = -1,
+                        Fulfillable = Fulfillable.Rejected_Unauthorised
+                    };
+                }
 
-            //}
+            }
 
             // Currently indicates standard "passed to volunteers" result
             response.Fulfillable = Fulfillable.Accepted_ManualReferral;
