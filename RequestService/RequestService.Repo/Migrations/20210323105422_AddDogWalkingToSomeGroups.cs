39040569006b500018ae6281c97b6a746fd0c79f
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-03-23 10:58:22 +0000

    Add dogwalking to balderton and northmuskham

---
+++ RequestService/RequestService.Repo/Migrations/20210323105422_AddDogWalkingToSomeGroups.cs
@@ -0,0 +1,232 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class AddDogWalkingToSomeGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 4, 1, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 4, 6, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 4, 10, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 4, 14, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 6, 1, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 6, 6, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 6, 10, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 6, 14, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 8, 1, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 8, 6, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 8, 10, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 8, 14, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 9, 1, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 9, 6, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 9, 10, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 9, 14, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 10, 1, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 10, 6, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 10, 10, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 10, 14, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 2, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 3, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 4, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 5, 12 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 14, 12 });
+
+            migrationBuilder.InsertData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                columns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID", "Location", "Order", "PlaceholderText", "RequestFormStageID", "Required", "Subtext" },
+                values: new object[,]
+                {
+                    { 5, 1, 9, "pos1", 1, "Please don’t include any sensitive details that aren’t needed in order for us to help you", 1, false, null },
+                    { 5, 10, 9, "details2", 2, "For example, any special instructions for the volunteer.", 2, false, "This information will be visible to volunteers deciding whether to accept the request" },
+                    { 5, 6, 9, "pos3", 2, null, 1, true, null },
+                    { 5, 14, 9, "details2", 3, "For example, a door entry code, or contact details for a friend / relative / caregiver.", 2, false, "We will only share this information with a volunteer after they have accepted your request" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 5, 1, 9 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 5, 6, 9 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 5, 10, 9 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 5, 14, 9 });
+
+            migrationBuilder.InsertData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                columns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID", "Location", "Order", "PlaceholderText", "RequestFormStageID", "Required", "Subtext" },
+                values: new object[,]
+                {
+                    { 10, 1, 12, "pos1", 1, "Please don’t include any sensitive details that aren’t needed in order for us to help you", 1, false, null },
+                    { 8, 10, 12, "details2", 2, "For example, any special instructions for the volunteer.", 2, false, "This information will be visible to volunteers deciding whether to accept the request" },
+                    { 8, 1, 12, "pos1", 1, "Please don’t include any sensitive details that aren’t needed in order for us to help you", 1, false, null },
+                    { 4, 14, 12, "details2", 3, "For example, a door entry code, or contact details for a friend / relative / caregiver.", 2, false, "We will only share this information with a volunteer after they have accepted your request" },
+                    { 4, 6, 12, "pos3", 2, null, 1, true, null },
+                    { 4, 10, 12, "details2", 2, "For example, any special instructions for the volunteer.", 2, false, "This information will be visible to volunteers deciding whether to accept the request" },
+                    { 4, 1, 12, "pos1", 1, "Please don’t include any sensitive details that aren’t needed in order for us to help you", 1, false, null },
+                    { 6, 14, 12, "details2", 3, "For example, a door entry code, or contact details for a friend / relative / caregiver.", 2, false, "We will only share this information with a volunteer after they have accepted your request" },
+                    { 6, 6, 12, "pos3", 2, null, 1, true, null },
+                    { 6, 10, 12, "details2", 2, "For example, any special instructions for the volunteer.", 2, false, "This information will be visible to volunteers deciding whether to accept the request" },
+                    { 6, 1, 12, "pos1", 1, "Please don’t include any sensitive details that aren’t needed in order for us to help you", 1, false, null },
+                    { 8, 6, 12, "pos3", 2, null, 1, true, null },
+                    { 9, 14, 12, "details2", 3, "For example, a door entry code, or contact details for a friend / relative / caregiver.", 2, false, "We will only share this information with a volunteer after they have accepted your request" },
+                    { 9, 10, 12, "details2", 2, "For example, any special instructions for the volunteer.", 2, false, "This information will be visible to volunteers deciding whether to accept the request" },
+                    { 9, 1, 12, "pos1", 1, "Please don’t include any sensitive details that aren’t needed in order for us to help you", 1, false, null },
+                    { 12, 14, 12, "details2", 3, "For example, a door entry code, or contact details for a friend / relative / caregiver.", 2, false, "We will only share this information with a volunteer after they have accepted your request" },
+                    { 12, 5, 12, "pos3", 4, null, 1, false, "Volunteers are providing their time and skills free of charge." },
+                    { 12, 4, 12, "pos3", 3, null, 1, false, null },
+                    { 12, 3, 12, "pos3", 1, null, 1, true, "Remember they’re washable and reusable, so only request what you need between washes." },
+                    { 12, 2, 12, "pos2", 2, "Don’t forget to tell us how many of each size you need. If you have very specific style requirements it may take longer to find a volunteer to help with your request. Please don’t include personal information such as name or address in this box, we’ll ask for that later.", 1, false, "Size guide:<br />&nbsp;- Men’s (Small / Medium / Large)<br />&nbsp;- Ladies’ (Small / Medium / Large)<br />&nbsp;- Children’s (One Size - under 12)" },
+                    { 10, 14, 12, "details2", 3, "For example, a door entry code, or contact details for a friend / relative / caregiver.", 2, false, "We will only share this information with a volunteer after they have accepted your request" },
+                    { 10, 6, 12, "pos3", 2, null, 1, true, null },
+                    { 10, 10, 12, "details2", 2, "For example, any special instructions for the volunteer.", 2, false, "This information will be visible to volunteers deciding whether to accept the request" },
+                    { 9, 6, 12, "pos3", 2, null, 1, true, null },
+                    { 8, 14, 12, "details2", 3, "For example, a door entry code, or contact details for a friend / relative / caregiver.", 2, false, "We will only share this information with a volunteer after they have accepted your request" }
+                });
+        }
+    }
+}
