2121df5f80a470f5e81adeda3c820d29309e848f
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-03-24 08:55:39 +0000

    Move RecipientAge question

---
+++ RequestService/RequestService.Repo/Migrations/20210324085317_MoveRecipientAgeQuestion.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class MoveRecipientAgeQuestion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 1, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 1, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 2, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 2, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 7, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 7, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 11, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 11, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 22, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 26, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 26, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 27, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 27, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "details1", 2 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "Question",
+                keyColumn: "ID",
+                keyValue: 18,
+                column: "QuestionType",
+                value: (byte)2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 1, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 1, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 2, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 2, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 7, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 7, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 11, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 11, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 22, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 26, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 26, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 27, 18, 20 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 27, 18, 21 },
+                columns: new[] { "Location", "RequestFormStageID" },
+                values: new object[] { "pos1", 1 });
+
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "Question",
+                keyColumn: "ID",
+                keyValue: 18,
+                column: "QuestionType",
+                value: (byte)1);
+        }
+    }
+}
