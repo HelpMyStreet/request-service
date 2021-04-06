8ce3ce0bea069a465703a3290944a16513354498
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-04-06 11:36:41 +0100

    change equality comparer

--- RequestService/RequestService.Handlers/GetAllJobsByFilterHandler.cs
+++ RequestService/RequestService.Handlers/GetAllJobsByFilterHandler.cs
@@ -24,7 +24,7 @@ namespace RequestService.Handlers
         private readonly IAddressService _addressService;
         private readonly IJobFilteringService _jobFilteringService;
         private readonly IGroupService _groupService;
-        private IEqualityComparer<JobBasic> _jobBasicDedupe_EqualityComparer;
+        private IEqualityComparer<JobBasic> _jobBasicDedupeWithDate_EqualityComparer;
 
         public GetAllJobsByFilterHandler(
             IRepository repository,
@@ -36,7 +36,7 @@ namespace RequestService.Handlers
             _addressService = addressService;
             _jobFilteringService = jobFilteringService;
             _groupService = groupService;
-            _jobBasicDedupe_EqualityComparer = new JobBasicDedupe_EqualityComparer();
+            _jobBasicDedupeWithDate_EqualityComparer = new JobBasicDedupeWithDate_EqualityComparer();
         }
 
         private async Task AddPostCodeForLocations(List<JobDTO> allJobs)
@@ -162,7 +162,7 @@ namespace RequestService.Handlers
                 var allocatedJobsToUsers = _repository.GetUserJobs(request.ExcludeSiblingsOfJobsAllocatedToUserID.Value);
                 if (allocatedJobsToUsers != null && allocatedJobsToUsers.Count() > 0)
                 {
-                    allFilteredJobs = allFilteredJobs.Where(s => !allocatedJobsToUsers.Contains(s, _jobBasicDedupe_EqualityComparer)).ToList();
+                    allFilteredJobs = allFilteredJobs.Where(s => !allocatedJobsToUsers.Contains(s, _jobBasicDedupeWithDate_EqualityComparer)).ToList();
                 }
             }
 
