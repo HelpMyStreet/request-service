e12966012b0131b8b921d34cea0b4bc3457e9279
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-01-06 12:08:58 +0000

    add change tracking for 3 tables

---
+++ RequestService/RequestService.Repo/Migrations/20220106114208_AddChangeTrackingToRequestSupportActivities.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class AddChangeTrackingToRequestSupportActivities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO [dbo].[table_store_ChangeTracking_version]([TableName],[SYS_CHANGE_VERSION]) SELECT 'Request.SupportActivities',0;
+                ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE [Request].[SupportActivities] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON)
+                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM [dbo].[table_store_ChangeTracking_version] WHERE TableName='Request.SupportActivities';                
+                ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE [Request].[SupportActivities] DISABLE CHANGE_TRACKING
+                ");
+        }
+    }
+}
---
+++ RequestService/RequestService.Repo/Migrations/20220106114725_AddChangeTrackingToUpdateHistor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class AddChangeTrackingToUpdateHistor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO [dbo].[table_store_ChangeTracking_version]([TableName],[SYS_CHANGE_VERSION]) SELECT 'Request.UpdateHistory',0;
+                ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE [Request].[UpdateHistory] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON)
+                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM [dbo].[table_store_ChangeTracking_version] WHERE TableName='Request.UpdateHistory';                
+                ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE [Request].[UpdateHistory] DISABLE CHANGE_TRACKING
+                ");
+        }
+    }
+}
